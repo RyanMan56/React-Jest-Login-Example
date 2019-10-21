a50ecdf091b93aa481099c8c5b4b05770a3de69e
Ryan <ryanman56@gmail.com>
2019-10-21 16:29:57 +0100

    added a couple of snapshots

--- src/components/common/avatar/Avatar.test.js
+++ src/components/common/avatar/Avatar.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import Avatar, { hashEmail } from './Avatar';
 
 it('renders without crashing', () => {  
   shallow(<Avatar email="test@test.com" />);
+});
+
+it('matches snapshot', () => {
+  const avatar = renderer
+    .create(
+      <Avatar email="test@test.com" />
+    ).toJSON();
+
+  expect(avatar).toMatchSnapshot();
 });
 
 it('successfully hashes email address', () => {
--- src/components/common/spinner/Spinner.test.js
+++ src/components/common/spinner/Spinner.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import Spinner from './Spinner';
 
@@ -6,3 +7,11 @@ import Spinner from './Spinner';
 it('shallowly renders without crashing', () => {
   shallow(<Spinner />);
 });
+
+it('matches snapshot', () => {
+  const spinner = renderer
+    .create(
+      <Spinner />
+    ).toJSON();
+  expect(spinner).toMatchSnapshot();
+})
